9b7d99f1c747e72de44b9217b9186cca138ab416
EunmiKang <hv19962000@gmail.com>
2019-05-22 11:05:03 +0900

    로딩 화면 코드 추가

---
+++ WebContent/js/fakeLoader.js
@@ -0,0 +1,24 @@
+
+function fakeLoader() {
+		var settings = $.extend({
+			targetClass : 'fakeLoader',
+			timeToHide : 5000,
+			bgColor : '#2ecc71',
+			spinner : 'spinner7'
+		});
+
+		var spinner07 = '<div class="fl fl-spinner spinner7"><div class="circ1"></div><div class="circ2"></div><div class="circ3"></div><div class="circ4"></div></div>';
+		var loaderDiv = $('body').find('.' + settings.targetClass);
+
+		loaderDiv.html(spinner07);
+
+		loaderDiv.css({
+			'display': "block",
+			'backgroundColor' : settings.bgColor
+		});
+
+		setTimeout(function () {
+            $(loaderDiv).fadeOut();
+        }, settings.timeToHide);
+	}
+
